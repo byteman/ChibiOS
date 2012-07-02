8b4e7be39a5cc8e08f62330d0b2fc6e02500f3b2
byteman <wangchen2011@gmail.com>
2012-07-02 23:19:22 +0800

    修改网络gpio设置和phyid

--- boards/DT_STM32_P107/board.h
+++ boards/DT_STM32_P107/board.h
@@ -22,14 +22,14 @@
 #define _BOARD_H_
 
 /*
- * Setup for the Olimex STM32-P107 Rev.A evaluation board.
+ * Setup for the STMicroelectronics STM3210C-EVAL evaluation board.
  */
 
 /*
  * Board identifier.
  */
-#define BOARD_OLIMEX_STM32_P107_REV_A
-#define BOARD_NAME              "Olimex STM32-P107 Rev.A"
+#define BOARD_START01_STM32_P107
+#define BOARD_NAME              "Start01 STM32-P107"
 
 /*
  * Board frequencies.
@@ -45,17 +45,19 @@
 /*
  * Ethernet PHY type.
  */
-#define BOARD_PHY_ID            MII_STE101P_ID
+#define BOARD_PHY_ID            MII_DM9161B_ID
 #define BOARD_PHY_RMII
 
 /*
  * IO pins assignments.
  */
-#define GPIOA_SWITCH_WKUP       0
-#define GPIOA_SPI3_CS_MMC       4
-#define GPIOC_LED_STATUS1       6
-#define GPIOC_LED_STATUS2       7
-#define GPIOC_SWITCH_TAMPER     13
+#define GPIOA_SPI1NSS           4
+
+#define GPIOE_LED_STATUS1       2
+#define GPIOE_LED_STATUS2       3
+#define GPIOE_LED_STATUS3       4
+#define GPIOE_LED_STATUS4       5
+#define GPIOE_SWITCH_TAMPER     6
 
 /*
  * I/O ports initial setup, this configuration is established soon after reset
@@ -87,23 +89,23 @@
  * PA0  - Normal input      (WKUP BUTTON).
  * PA1  - Normal input      (ETH_RMII_REF_CLK).
  * PA2  - Alternate output  (ETH_RMII_MDIO).
- * PA3  - Input with PU     (unconnected).
- * PA4  - Open Drain output (CS_MMC).
- * PA5  - Input with PU     (unconnected).
- * PA6  - Input with PU     (unconnected).
- * PA7  - Normal input      (ETH_RMII_CRS_DV).
+ * PA3  - Push Pull output    (FM25L16 WP).
+ * PA4  - Alternate output  (FM25L16 CS).
+ * PA5  - Alternate output     (SPI1 SCK).
+ * PA6  - Input with PU     (SPI1 MISO).
+ * PA7  - Alternate output       (SPI1 MOSI).
  * PA8  - Alternate output  (MCO).
- * PA9  - Normal input      (OTG_VBUS).
- * PA10 - Normal input      (OTG_ID).
+ * PA9  - Alternate output      (usart1 tx).
+ * PA10 - Normal input      (usart1 rx).
  * PA11 - Normal input      (OTG_DM).
  * PA12 - Normal input      (OTG_DP).
  * PA13 - Normal input      (TMS).
  * PA14 - Normal input      (TCK).
  * PA15 - Normal input      (TDI).
  */
-#define VAL_GPIOACRL            0x48878B44      /*  PA7...PA0 */
-#define VAL_GPIOACRH            0x4444444B      /* PA15...PA8 */
-#define VAL_GPIOAODR            0xFFFFFFFF
+#define VAL_GPIOACRL            0xB8BB3B44      /*  PA7...PA0 */
+#define VAL_GPIOACRH            0x444444BB      /* PA15...PA8 */
+#define VAL_GPIOAODR            0xFFFFFFBF
 
 /*
  * Port B setup:
@@ -115,17 +117,17 @@
  * PB5  - Input with PU     (unconnected).
  * PB6  - Input with PU     (unconnected).
  * PB7  - Input with PU     (unconnected).
- * PB8  - Alternate O.D.    (I2C1 SCL, remapped).
- * PB9  - Alternate O.D.    (I2C1 SDA, remapped).
+ * PB8  - Input with PU    (unconnected).
+ * PB9  - Input with PU    (unconnected).
  * PB10 - Input with PU     (unconnected).
  * PB11 - Alternate output  (ETH_RMII_TX_EN).
  * PB12 - Alternate output  (ETH_RMII_TXD0).
  * PB13 - Alternate output  (ETH_RMII_TXD1).
  * PB14 - Input with PU     (unconnected).
- * PB15 - Push Pull output  (CS_UEXT).
+ * PB15 - Input with PU     (unconnected).
  */
 #define VAL_GPIOBCRL            0x88844488      /*  PB7...PB0 */
-#define VAL_GPIOBCRH            0x38BBB8FF      /* PB15...PB8 */
+#define VAL_GPIOBCRH            0x88BBB888      /* PB15...PB8 */
 #define VAL_GPIOBODR            0xFFFFFFFF
 
 /*
@@ -134,21 +136,21 @@
  * PC1  - Alternate output  (ETH_MDC).
  * PC2  - Input with PU     (unconnected).
  * PC3  - Input with PU     (unconnected).
- * PC4  - Normal input      (ETH_RMII_RXD0).
- * PC5  - Normal input      (ETH_RMII_RXD1).
- * PC6  - Push Pull output  (STAT1 green LED).
- * PC7  - Push Pull output  (STAT2 yellow LED).
+ * PC4  - Input with PU      (unconnected).
+ * PC5  - Input with PU      (MII_INT).
+ * PC6  - Input with PU  	(unconnected).
+ * PC7  - Input with PU  	(unconnected).
  * PC8  - Input with PU     (unconnected).
  * PC9  - Input with PU     (unconnected).
- * PC10 - Alternate output  (SPI3 SCK).
- * PC11 - Input with PU     (SPI3 MISO).
- * PC12 - Alternate output  (SPI3 MOSI).
- * PC13 - Normal input      (TAMPER).
+ * PC10 - Alternate output  (usart3 tx).
+ * PC11 - Normal input     (usart3 rx).
+ * PC12 - Push Pull output   (cmbus tx|rx|en).
+ * PC13 - Normal input      (unconnected).
  * PC14 - Normal input      (OSC32 IN).
  * PC15 - Normal input      (OSC32 OUT).
  */
-#define VAL_GPIOCCRL            0x334488B8      /*  PC7...PC0 */
-#define VAL_GPIOCCRH            0x444B8B88      /* PC15...PC8 */
+#define VAL_GPIOCCRL            0x888888B8      /*  PC7...PC0 */
+#define VAL_GPIOCCRH            0x44434B88      /* PC15...PC8 */
 #define VAL_GPIOCODR            0xFFFFFF3F
 
 /*
@@ -160,28 +162,41 @@
  * PD4  - Input with PU     (unconnected).
  * PD5  - Alternate output  (USART2 TX, UEXT).
  * PD6  - Input with PU     (USART2 RX, UEXT).
- * PD7  - Push Pull output  (USB_VBUSON).
- * PD8  - Alternate output  (USART2 TX, remapped).
- * PD9  - Normal input      (USART2 RX, remapped).
- * PD10 - Input with PU     (unconnected).
- * PD11 - Normal input      (USART2 CTS, remapped).
- * PD12 - Alternate output  (USART2 RTS, remapped).
+ * PD7  - Input with PU  	(unconnected).
+ * PD8  - Normal input   	(ETH CRS_DV, remapped).
+ * PD9  - Normal input      (ETH RXD0,   remapped).
+ * PD10 - Normal input      (ETH RXD1,   remapped).
+ * PD11 - Input with PU     (unconnected).
+ * PD12 - Input with PU  	(unconnected).
  * PD13 - Input with PU     (unconnected).
- * PD14 - Input with PU     (unconnected).
- * PD15 - Input with PU     (unconnected).
+ * PD14 - Push Pull output     (RST anybus).
+ * PD15 - Push Pull output     (MD anybus).
  */
-#define VAL_GPIODCRL            0x38B88888      /*  PD7...PD0 */
-#define VAL_GPIODCRH            0x888B484B      /* PD15...PD8 */
+#define VAL_GPIODCRL            0x88B88888      /*  PD7...PD0 */
+#define VAL_GPIODCRH            0x338B4444      /* PD15...PD8 */
 #define VAL_GPIODODR            0xFFFFFFFF
-
 /*
- * Port E setup.
- * Everything input with pull-up except:
- * PE14 - Normal input      (ETH_RMII_MDINT).
- * PE15 - Normal input      (USB_FAULT).
+ * Port E setup:
+ * PE0  - Input with PU     (unconnected).
+ * PE1  - Input with PU     (unconnected).
+ * PE2  - Push Pull output     (LED1).
+ * PE3  - Push Pull output     (LED2).
+ * PE4  - Push Pull output     (LED3).
+ * PE5  - Push Pull output  (LED4).
+ 
+ * PE6  - Normal input      (KEY).
+ * PE7 - Input with PU     (unconnected).
+ * PE8 - Input with PU     (unconnected).
+ * PE9 - Input with PU     (unconnected).
+ * PE10 - Input with PU     (unconnected).
+ * PE11 - Input with PU     (unconnected).
+ * PE12 - Input with PU     (unconnected).
+ * PE13 - Input with PU     (unconnected).
+ * PE14 - Input with PU     (unconnected).
+ * PE15 - Input with PU     (unconnected).
  */
-#define VAL_GPIOECRL            0x88888888      /*  PE7...PE0 */
-#define VAL_GPIOECRH            0x44888888      /* PE15...PE8 */
+#define VAL_GPIOECRL            0x84333388      /*  PE7...PE0 */
+#define VAL_GPIOECRH            0x88888888      /* PE15...PE8 */
 #define VAL_GPIOEODR            0xFFFFFFFF
 
 #if !defined(_FROM_ASM_)
